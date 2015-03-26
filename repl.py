c437e121912a2e051081e59e930c15e72130eea9
Finn Ellis <relsqui@chiliahedron.com>
2015-03-26 12:19:51 -0700

    Add player name, save with pickle.

--- repl.py
+++ repl.py
@@ -2,14 +2,20 @@
 
 import time
 import sys
+import pickle
+
+
+class Player(object):
+    def __init__(self, name):
+        self.name = name
+
 
 class Clock(object):
-    ticks_hour = 100
-    ticks_day = 1200 # 12 hours/day
-    ticks_month = 33600 # 28 days/month
-    ticks_year = 403200 # 12 months/year
-
     def __init__(self, seed=None, speed=None):
+        self.ticks_hour = 100
+        self.ticks_day = 1200 # 12 hours/day
+        self.ticks_month = 33600 # 28 days/month
+        self.ticks_year = 403200 # 12 months/year
         if seed is None:
             seed = int(time.time())
         self.seed = seed
@@ -33,37 +39,59 @@ class Clock(object):
         return time_string.format(y=self.year, m=self.month, d=self.day, h=self.hour, n=self.minute)
 
 
-try:
-    with open("savefile", "r") as f:
-        try:
-            seed = int(f.read())
+class Game(object):
+    def __init__(self, player, clock):
+        self.player = player
+        self.clock = clock
+
+    def loop(self):
+        while True:
+            line = raw_input("> ")
+            self.clock.update()
+            if line == "quit":
+                break
+            elif line.startswith("name "):
+                name = line.split(None, 1)[1]
+                self.player.name = name
+                print("OK. Hi, {n}.".format(n=name))
+            elif line == "tick":
+                print(self.clock.tick)
+            elif line == "time":
+                print(self.clock.time())
+            elif line == "who":
+                print("You are {n}.".format(n=self.player.name))
+            else:
+                print("OK ({l})".format(l=line))
+
+    def save(self):
+        with open("savefile", "w") as f:
+            pickle.dump(self, f, 0)
+            print("Saved. Goodbye!")
+
+
+def load(filename=None):
+    if filename is None:
+        filename = "savefile"
+    try:
+        with open(filename, "r") as f:
+            game = pickle.load(f)
             print("Welcome back!")
-        except ValueError:
-            print("Couldn't parse savefile. Quitting, just to be safe.")
-            sys.exit(1)
-except IOError as e:
-    if e.errno == 2:
-        # File doesn't exist.
-        print("Welcome!")
-        seed = None
-    else:
-        print("Couldn't read savefile ('{e}'). Quitting, just to be safe.".format(e=e.strerror))
-        sys.exit(2)
+            return game
+    except IOError as e:
+        if e.errno == 2:
+            # File doesn't exist.
+            print("Welcome! What shall I call you?")
+            name = raw_input("% ")
+            print("Hi, {n}.".format(n=name))
+            return Game(Player(name), Clock())
+        else:
+            print("Couldn't read savefile ('{e}'). Quitting, just to be safe.".format(e=e.strerror))
+            sys.exit(2)
 
-clock = Clock(seed=seed)
-clock.update()
-while True:
-    line = raw_input("> ")
-    clock.update()
-    if line == "quit":
-        break
-    elif line == "tick":
-        print(clock.tick)
-    elif line == "time":
-        print(clock.time())
-    else:
-        print("OK ({})".format(line))
+def main():
+    game = load()
+    game.loop()
+    game.save()
 
-with open("savefile", "w") as f:
-    f.write(str(clock.seed))
-    print("Saved. Goodbye!")
+if __name__ == "__main__":
+    main()
